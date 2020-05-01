6cabb636c6abf3125bc4b2678cc9e9687a625c11
Ottoedge Mumbai <ottoedgemumbai@Karans-MacBook-Air.local>
2020-05-02 00:09:00 +0530

    Added Some Content

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+import {Icon }from 'react-native-vector-icons/FontAwesome';
+import { CheckBox } from 'react-native-elements';
+import { PricingCard } from 'react-native-elements';
+
+import { Header } from 'react'
+
+
+
 
 export default function App() {
   return (
+  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!HOLA </Text>
+   
+      <Text>Welcome To Our Booking App </Text>
+
+
+     
+    <PricingCard
+    color="#4f9deb"
+    title="Sale"
+    price="$69"
+    info={['1 User', 'Basic Support', 'All Core Features']}
+    button={{ title: 'GET STARTED',icon: 'flight-takeoff'  }}
+    />
+
+<Button
+  
+  title="Book Tickets"
+  
+/>
+
     </View>
+  
   );
 }
 
