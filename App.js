5648d0ee5cd5decbb434aa91cfb8b1066eca0395
Ottoedge Mumbai <ottoedgemumbai@Karans-MacBook-Air.local>
2020-05-02 03:14:11 +0530

    Latest edit

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text,Alert,View } from 'react-native';
-import { Button } from 'react-native-elements';
+import { Button,Overlay } from 'react-native-elements';
 import {Icon }from 'react-native-vector-icons/FontAwesome';
 import { CheckBox } from 'react-native-elements';
 import { PricingCard } from 'react-native-elements';
+import { Slider } from 'react-native-elements';
+import { Tooltip } from 'react-native-elements';
 
 import { Input } from 'react-native-elements';
 
@@ -12,7 +14,7 @@ function HomeScreen() {
 return(
   console.log("Hellow World")
        );
-} //DO IN TERMINAL NAAAA  stop 
+} 
 export default function App() {
  
   return (
@@ -40,12 +42,17 @@ export default function App() {
     <PricingCard
     color="#4f9deb"
     title="Sale"
-    price="$200"
+    price="$2001"
     info={['1 User', 'Basic Support', 'All Core Features..']}
     button={{ title: 'GET STARTED',icon: 'flight-takeoff'  }}
     />
 
-
+<View style={{ flex: 0, alignItems: 'stretch', justifyContent: 'center' }}>
+  <Slider
+ 
+  />
+  <Tooltip popover={<Text>Nice to Meet you</Text>}><Text>Click Me</Text></Tooltip>
+</View>
 
     </View>
  
